5fa419bf232075e17de42a8429b19e9cc1aa98b3
DanielVenzi <gomesvenzi@gmail.com>
2020-06-26 18:06:41 -0300

    Adicionando o agendamento diario as 23:00

--- zabbix_cleaner/zabbix_cleaner.py
+++ zabbix_cleaner/zabbix_cleaner.py
@@ -1,35 +1,51 @@
 import os
 import mysql.connector
+import schedule
+import time
 
-try:
-    print("Zabbix cleaner - Inicializando a conexão...")
-    connection = mysql.connector.connect(host='127.0.0.1',
-                                        database='zabbix',
-                                        user='root',
-                                        password='Zabbix!22')
+def zabbix_clean():
 
-    if connection.is_connected():
-        print("Zabbix - cleaner - Conexão bem sucedida")
-        cursor = connection.cursor()
-        sql_Delete_queries = ["DELETE FROM history_uint;",
-                                "DELETE FROM history;",
-                                "DELETE FROM trends_uint;",
-                                "DELETE FROM trends;",
-                                "DELETE FROM events;",
-                                "DELETE FROM alerts;"]
+    try:
+        print("Zabbix cleaner - Inicializando a conexão...")
+        connection = mysql.connector.connect(host='127.0.0.1',
+                                            database='zabbix',
+                                            user='root',
+                                            password='Zabbix!22')
 
-        for query  in sql_Delete_queries:
-            print("Zabbix cleaner - Executando comando: {}".format(query))
-            cursor.execute(query)
+        if connection.is_connected():
+            print("Zabbix - cleaner - Conexão bem sucedida")
+            cursor = connection.cursor()
+            sql_Delete_queries = ["DELETE FROM history_uint;",
+                                    "DELETE FROM history;",
+                                    "DELETE FROM trends_uint;",
+                                    "DELETE FROM trends;",
+                                    "DELETE FROM events;",
+                                    "DELETE FROM alerts;"]
 
-        connection.commit()
-        cursor.close()
-        connection.close()
-        print("Zabbix cleaner - Registros antigos retirados do banco de dados.")
-    
-    else:
-        print("Zabbix - cleaner - Conexão mal sucedida")
+            for query  in sql_Delete_queries:
+                print("Zabbix cleaner - Executando comando: {}".format(query))
+                cursor.execute(query)
+
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print("Zabbix cleaner - Registros antigos retirados do banco de dados.")
         
+        else:
+            print("Zabbix - cleaner - Conexão mal sucedida")
+            
 
-except mysql.connector.Error as e:
-    print("Zabbix cleaner - Um erro aconteceu {}".format(e))
+    except mysql.connector.Error as e:
+        print("Zabbix cleaner - Um erro aconteceu {}".format(e))
+
+if __name__ == "__main__":
+
+    schedule.every().monday.at("23:00").do(zabbix_clean)
+    schedule.every().tuesday.at("23:00").do(zabbix_clean)
+    schedule.every().wednesday.at("23:00").do(zabbix_clean)
+    schedule.every().thursday.at("23:00").do(zabbix_clean)
+    schedule.every().friday.at("23:00").do(zabbix_clean)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
